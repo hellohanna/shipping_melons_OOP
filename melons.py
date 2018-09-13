924abe3a3a6a0d8c7c8347db09f0818fce0219aa
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-13 16:00:28 -0700

    Updated Christmas melons cost and added a flat fee to international orders < 10 melons

--- melons.py
+++ melons.py
@@ -4,7 +4,7 @@
 class AbstractMelonOrder():
     """An abstract base class that other Melon Orders inherit from."""
 
-    def __init__(self, species, qty, country_code = None):
+    def __init__(self, species, qty, country_code):
 
         self.species = species
         self.qty = qty
@@ -13,26 +13,29 @@ class AbstractMelonOrder():
 
     def get_total(self):
         """Calculate price, including tax."""
-
         base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
+        fee = 0
+
+        if self.species.title() == "Christmas":
+            base_price = 1.5*base_price
+
+        if self.order_type == "international" and self.qty < 10:
+            fee = 3
+
+        total = (1 + self.tax) * self.qty * base_price + fee
 
         return total
 
     def mark_shipped(self):
         """Record the fact than an order has been shipped."""
 
-        self.shipped = True
-
-       
+        self.shipped = True       
 
 
 class DomesticMelonOrder(AbstractMelonOrder):
     """A melon order within the USA."""
     order_type = "domestic"
-    tax = 0.08
-    
-    
+    tax = 0.08  
 
 
 class InternationalMelonOrder(AbstractMelonOrder):
